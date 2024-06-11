dea5e6e5f38a40a8e65fef548e8e1f4230437b0b
Lodeki-Jerry <elodeki@gmail.com>
2024-06-11 12:14:35 +0300

    updated

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -2,6 +2,7 @@ import BookingTable from "../features/bookings/BookingTable";
 import BookingTableOperations from "../features/bookings/BookingTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+// Booking page
 
 function Bookings() {
   return (
